38b9a7457b42dcbea7516c05f8d0e46d57549219
AJAYKUMAR <AJAYKUMAR@DESKTOP-CKH93SC>
2023-12-21 16:56:00 +0530

    Add project files.

---
+++ WindowsFormsApp1/login form.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Enter the username");
+            }
+
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Enter the passward");
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection("Data Source=DESKTOP-CKH93SC;Initial Catalog=login;Integrated Security=True");
+                    SqlCommand cmd = new SqlCommand("select * from table_login where username=@username and passward=@passward", con);
+                    cmd.Parameters.AddWithValue("@username", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@passward", textBox2.Text);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        MessageBox.Show("login successfull");
+                    }
+                    else
+                    {
+                        MessageBox.Show("username or passward is invalid");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+
+                }    }
+            
+            }   
+    }
+}
